a54830b61cb204ea608cad23b6167acca343acf7
kafku <kazunoko93@gmail.com>
2019-02-12 15:59:58 +0900

    add some matrix-vector operations

--- v.h
+++ v.h
@@ -56,9 +56,12 @@ namespace v {
 		while (--m >= 0) (*xd++) += ((*yd++) - (*zd++));
 	}
 #endif
+	inline float norm(const Vector& x) {
+		return std::sqrt(dot(x, x));
+	}
 
 	inline void unit(Vector& x) {
-		float len = ::sqrt(dot(x, x));
+		const float len = norm(x);
 		if (len == 0) return;
 
 		int m = x.size(); float *xd = x.data();
@@ -99,7 +102,59 @@ namespace v {
 		const int n_rows, n_cols;
 	};
 
+	// x^T A y
+	template <typename Vector1, typename Vector2>
+	float dot_A(const Vector1& x, const Vector2& y, const LightMatrix& A) {
+		float sum_all = 0.0;
+		const float *xd = x.data();
+		for (int row = 0; row < A.n_rows; ++row, ++xd) {
+			int n_cols = A.n_cols;
+			const float *Ad = A.row(row).data(), *yd = y.data();
+			float sum = 0.0;
+			while (--n_cols >= 0) sum += (*Ad++) * (*yd++);
+			sum_all = *xd * sum;
+		}
+		return sum_all;
+	}
+
+	// norm(Ax)
+	float sgemv_norm(const LightMatrix& A, const Vector& x) {
+		float sum_all = 0.0;
+		for (int row = 0; row < A.n_rows; ++row) {
+			int n_cols = A.n_cols;
+			const float *Ad = A.row(row).data(), *xd = x.data();
+			float sum = 0.0;
+			while (--n_cols >= 0) sum += (*Ad++) * (*xd++);
+			sum_all += sum * sum;
+		}
+		return std::sqrt(sum_all);
+	}
+
 #if 0
+	inline void sgemv(const LightMatrix& A, const LightVector& x, LightVector& y) {
+		for (int row = 0; row < A.n_rows; ++row) {
+			int n_cols = A.n_cols;
+			const float *Ad = A.row(row).data(), *xd = x.data();
+			float sum = 0.0;
+			while (--n_cols >= 0) sum += (*Ad++) * (*xd++);
+			y[row] = sum;
+		}
+	}
+
+	inline void sger(float alpha, const LightVector& x, const LightVector& y, LightMatrix& A) {
+		const float *xd = x.data();
+		for (int row = 0; row < A.n_rows; ++row) {
+			int n_cols = A.n_cols;
+			float *Ad = A.row(row).data();
+			const float *yd = y.data();
+			while (--n_cols >= 0) {
+				*Ad += *xd * *yd * alpha;
+				++Ad, ++yd;
+			}
+			++xd;
+		}
+	}
+
 	inline float dot(const Vector&x, const Vector& y) {
 		int m = x.size(); const float *xd = x.data(), *yd = y.data();
 		float sum = 0.0;
