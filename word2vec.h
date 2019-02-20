ccc53eab50c3370f4a8e0cf1fba98bdc13b9200b
kafku <kazunoko93@gmail.com>
2019-02-20 19:37:02 +0900

    split file

--- word2vec.h
+++ word2vec.h
@@ -23,45 +23,10 @@
 
 #include "v.h"
 #include "param_update.h"
+#include "cvt.h"
 
 #include "model_generated.h"
 
-template <typename T> struct Cvt;
-
-template <> struct Cvt<std::string> {
-	static const std::string& to_utf8(const std::string& s) { return s; }
-	static const std::string& from_utf8(const std::string& s) { return s; }
-};
-
-#if defined(_LIBCPP_BEGIN_NAMESPACE_STD)
-#include <codecvt>
-template <> struct Cvt<std::u16string> {
-	static std::string to_utf8(const std::u16string& in) {
-	    std::wstring_convert<std::codecvt_utf8<char16_t>, char16_t> cv;
-    	return cv.to_bytes(in.data());
-	}
-
-	static std::u16string from_utf8(const std::string& in) {
-	    std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> cv;
-    	return cv.from_bytes(in.data());
-	}
-};
-#else // gcc has no <codecvt>
-#include "utf8cpp/utf8.h"
-template <> struct Cvt<std::u16string> {
-	static std::string to_utf8(const std::u16string& in) {
-		std::string out;
-		utf8::utf16to8(in.begin(), in.end(), std::back_inserter(out));
-		return out;
-	}
-
-	static std::u16string from_utf8(const std::string& in) {
-		std::u16string out;
-		utf8::utf8to16(in.begin(), in.end(), std::back_inserter(out));
-		return out;
-	}
-};
-#endif
 
 template <class String = std::string>
 struct Word2Vec
@@ -274,7 +239,7 @@ struct Word2Vec
 			size_t len = sentence->tokens_.size();
 			for (size_t i=0; i<len; ++i) {
 				auto it = vocab_.find(sentence->tokens_[i]);
-				if (it == vocab_.end()) continue;
+				if (it == vocab_.end()) continue; // skip OOV
 				Word *word = it->second.get();
 				// subsampling
 				if (sample_ > 0) {
