c60903cfd6c7479b96399d2b96fcd72c02a5862a
kafku <kazunoko93@gmail.com>
2019-02-21 12:00:55 +0900

    cosine similarity policy

--- grad_utils.h
+++ grad_utils.h
@@ -1,39 +1,65 @@
 #pragma once
 
 #include <cmath>
+#include <Eigen/Core>
+#include <Eigen/Dense>
 #include "v.h"
 
 
 namespace gu {
 	using namespace v;
 
-	// grad = d cos(x, y) / d x
-	void grad_cos_wrt_vec(const Vector& x, const Vector& y, Vector& grad) {
-		const float norm_x_inv = 1.0 / norm(x), norm_y_inv = 1.0 / norm(y);
-		const float a = norm_x_inv * norm_y_inv; // 1.0 / (norm_x * norm_y)
-		const float b = dot(x, y) * std::pow(norm_x_inv, 3) * norm_y_inv; // dot(x, y) / (norm_x^3 ^ norm_y)
-		int m = x.size();
+	template <typename T>
+	struct CosSim
+	{
+		using VectorXT = Eigen::Matrix<T, Eigen::Dynamic, 1>;
+		using MatrixXT = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
 
-		// y / (norm_x * norm_y) - dot(x, y) x / (norm_x^3 * norm_y)
-		float *gd = grad.data();
-		const float *xd = x.data(), *yd = y.data();
-		while (--m >= 0)
-			*(gd++) = *(yd++) * a - *(xd++) * b;
-	}
+		// cos(y, Mx)
+		static T call(const Vector& y, const Vector& x, const LightMatrix& M) {
+			Eigen::Map<const VectorXT>(y.data(), y.size());
+			Eigen::Map<const VectorXT>(x.data(), x.size());
+			Eigen::Map<const MatrixXT>(M.data(), M.n_rows, M.n_cols);
 
-	// grad = d cons(y, Mx) / d x
-	void grad_cos_wrt_mat(const Vector& x, const Vector& y, const LightMatrix& M, LightMatrix& grad) {
-		const float norm_Mx_inv = 1.0 / sgemv_norm(M, x), norm_y_inv = 1.0 / norm(y);
-		const float a = norm_Mx_inv * norm_y_inv; // 1.0 / (norm(Mx) * norm(y))
-		const float b = dot_A(y, x, M) * std::pow(norm_Mx_inv, 3) * norm_y_inv; // y^T Mx / (norm(Mx)^3 * norm(y))
+			const MatrixXT Mx = M * x;
+			const T norm_Mx_inv = 1.0 / Mx.squaredNorm();
+			const T norm_y_inv = 1.0 / y.squaredNorm();
 
-		// y x^T / (norm(Mx) * norm(y)) - y^T Mx Mxx^T / (norm(Mx)^3 * norm(y))
-		float *gd = grad.data();
-		const float *xd = x.data(), *yd = y.data();
-		for (int row = 0; row < grad.n_rows; ++row) {
-			for (int col = 0; col < grad.n_cols; ++col) {
-				grad[row * grad.n_cols + col] = yd[row] * xd[col] * a - dot(M.row(row), x) * x[col] * b;
-			}
+			return (y.transpose() * Mx) * norm_Mx_inv * norm_y_inv;
 		}
-	}
+
+		// grad += alpha * d cos(Mx, y) / d x
+		static void grad_wrt_vec(const T alpha, const Vector& x, const Vector& y, const LightMatrix& M, Vector& grad) {
+			Eigen::Map<const VectorXT>(y.data(), y.size());
+			Eigen::Map<const VectorXT>(x.data(), x.size());
+			Eigen::Map<const MatrixXT>(M.data(), M.n_rows, M.n_cols);
+			Eigen::Map<VectorXT>(grad.data(), grad.size());
+
+			const MatrixXT Mx = M * x;
+			const T norm_Mx_inv = 1.0 / Mx.squaredNorm();
+			const T norm_y_inv = 1.0 / y.squaredNorm();
+			const T a = norm_Mx_inv * norm_y_inv; // 1.0 / (norm(x) * norm(y))
+			const T b = (y.transpose() * Mx) * std::pow(norm_Mx_inv, 3) * norm_y_inv; // dot(Mx, y) / (norm(Mx)^3 ^ norm(y))
+
+			// alpha * (M^T y / (norm(Mx) * norm(y)) - dot(x, y) M^T Mx / (norm(Mx)^3 * norm(y)))
+			grad += alpha * M.transpose() * (y * a - Mx * b);
+		}
+
+		// grad += alpha * d cos(y, Mx) / d M
+		static void grad_wrt_mat(const T alpha, const Vector& x, const Vector& y, const LightMatrix& M, LightMatrix& grad) {
+			Eigen::Map<const Eigen::VectorX<T>>(y.data(), y.size());
+			Eigen::Map<const Eigen::VectorX<T>>(x.data(), x.size());
+			Eigen::Map<const Eigen::MatrixX<T>>(M.data(), M.n_rows, M.n_cols);
+			Eigen::Map<Eigen::MatrixX<T>>(grad.data(), grad.n_rows, grad.n_cols);
+
+			const Eigen::MatrixX<T> Mx = M * x;
+			const T norm_Mx_inv = 1.0 / Mx.squaredNorm();
+			const T norm_y_inv = 1.0 / y.squaredNorm();
+			const T a = norm_Mx_inv * norm_y_inv; // 1.0 / (norm(Mx) * norm(y))
+			const T b = (y.transpose() * Mx) * std::pow(norm_Mx_inv, 3) * norm_y_inv; // y^T Mx / (norm(Mx)^3 * norm(y))
+
+			// grad += alpha * (y x^T / (norm(Mx) * norm(y)) - y^T Mx Mxx^T / (norm(Mx)^3 * norm(y)))
+			grad += alpha * (y * a - Mx * b) * x.transpose();
+		}
+	};
 } // end namespace: gu
